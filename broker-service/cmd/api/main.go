e8642695d50b5134d1e7110fa946c1c5833c150c
Ahmad-mufied <teranggelap01@gmail.com>
2023-10-09 23:09:32 +0700

    13. Building a docker image for the Broker service

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPORT = "8080"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %s", webPORT)
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPORT),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
